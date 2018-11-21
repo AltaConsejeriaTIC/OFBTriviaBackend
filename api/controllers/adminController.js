4a3a29a9f9cd0b993861828ef9cd13da9236b20f
Eduardo Galeano <cegard689@gmail.com>
2018-11-21 18:03:10 -0500

    Fixed bugs made by last refactor.

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -4,32 +4,43 @@ const bcrypt = require('bcrypt');
 const Admin = require('../models/Admin');
 const helpers = require('../helpers/helpers');
 
-function validateCryptedPassword(plainText, password, AdminId, res){
+function validateCryptedPassword(plainText, password, adminId, res){
 	bcrypt.compare(plainText, password, function(err, compareResult) {
-    helpers.runFunctionByCondition(compareResult, res.status(200).send, [{id: AdminId}],
-                                   res.status(401).send, [{message: "wrong password"}]);
-  });
+		
+		if(compareResult)
+			res.status(200).send({id: adminId});
+		
+		else
+			res.status(401).send({message: "wrong password"});
+	});
 }
 
 function validateLoginCredentials(req, res){
 	Admin.query().
-	select('admin_password as  password', 'admin_is_active as isActive').
+	select('admin_password as  password', 'admin_is_active as isActive', 'admin_id as id').
 	where('admin_account_name', req.body.userName).
 	andWhere('admin_is_active', true).
-	then(accountInfo =>
-		helpers.runFunctionByCondition(Boolean(accountInfo[0]), validateCryptedPassword,
-                                   [req.body.password, accountInfo[0].password,
-                                    accountInfo.id, res], res.status(401).send,
-                                   [{message: "wrong or deactivated account user"}]));
+	then(accountInfo => {
+    
+		if (accountInfo[0])
+			validateCryptedPassword(req.body.password, accountInfo[0].password, accountInfo[0].id, res);
+		
+		else
+			res.status(401).send({message: "wrong or deactivated account user"});
+	});
 }
 
 function registerAdmin(req, res){
 	Admin.query().
   insert(helpers.formatAdminData(req.body)).
-  then(admin =>
-    helpers.runFunctionByCondition(Boolean(admin), res.status(201).send,
-                                   [{message: "Admin created."}], res.status(500).send,
-                                   [{message: "Error on creating admin"}])).
+  then(admin => {
+    console.log(admin);
+    if(admin)
+      res.status(201).send({message: "Admin created."});
+    
+    else
+      res.status(500).send({message: "Error on creating admin"});
+  }).
   catch(e => console.log(e));
 }
 
