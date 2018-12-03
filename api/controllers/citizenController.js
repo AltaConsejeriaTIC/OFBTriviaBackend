cacc59ad559fb3d3dad0f904d973868eeee563fc
Eduardo Galeano <cegard689@gmail.com>
2018-12-03 05:22:29 -0500

    Some requests added (not deleting functions added).

--- api/controllers/citizenController.js
+++ api/controllers/citizenController.js
@@ -3,6 +3,19 @@
 const Citizen = require('../models/Citizen');
 const Question = require('../models/Question');
 const helpers = require('../helpers/helpers');
+const constants = require('../helpers/constants');
+
+function formatCitizenToSend(citizen){
+	
+	return {
+		id: citizen.citizen_id,
+		name: citizen.citizen_name,
+		lastName: citizen.citizen_last_name,
+		email: citizen.email,
+		contactPreference: citizen.contact_media,
+		cellphone: citizen.citizen_cellphone
+	};
+}
 
 function updateCitizen(citizenId, params){
 	
@@ -57,7 +70,16 @@ function getWinners(req, res) {
   }));
 }
 
+function getCitizen(req, res){
+	Citizen.query().
+	select().
+	where('citizen_id', req.swagger.params.id.value).
+	then(citizen => res.status(200).send(formatCitizenToSend(citizen[0]))).
+	catch(() => res.status(500).send(constants.errorMessage));
+}
+
 module.exports = {
   getWinners: getWinners,
-  processCitizen: processCitizen
+  processCitizen: processCitizen,
+  getUser: getCitizen
 };
